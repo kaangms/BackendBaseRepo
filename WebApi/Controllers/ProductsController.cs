9a48bd5c92afc7f2e5e7a1867519d029c4729884
Kaan Gümüş <54083292+kaangms@users.noreply.github.com>
2020-09-29 01:34:06 +0300

    Update ProductsController.cs

--- WebApi/Controllers/ProductsController.cs
+++ WebApi/Controllers/ProductsController.cs
@@ -75,7 +75,7 @@ namespace WebApi.Controllers
                 return BadRequest(result.Message);
             }
         }
-        [HttpPost("Update")]
+        [HttpPost("update")]
         public IActionResult Update(Product product)
         {
             var result = _productService.Update(product);
@@ -88,7 +88,7 @@ namespace WebApi.Controllers
                 return BadRequest(result.Message);
             }
         }
-        [HttpPost("Delete")]
+        [HttpPost("delete")]
         public IActionResult Delete(Product product)
         {
             var result = _productService.Delete(product);
